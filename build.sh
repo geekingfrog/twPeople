b2787548e854627fa33e481c7194e08b5acebff6
geekingfrog <greg@geekingfrog.com>
2013-03-28 22:20:23 +0800

    production ready, massive use of cdnjs

--- build.sh
+++ build.sh
@@ -4,5 +4,12 @@ echo "Make java from coffee"
 coffee -c app/
 
 echo "Compressing scripts"
-uglifyjs app/js/handlebars.js app/js/d3.v3.js app/js/celeb.js > app/js/prod.js
+uglifyjs app/js/celeb.js > app/js/prod.js
+# cat app/js/handlebars.js app/js/celeb.js > app/js/prod.js
+
+echo "Compress the css"
+cat app/foundation/css/normalize.css app/css/almost-flat-ui.css app/css/celeb.css > prodtmp.css
+sqwish prodtmp.css -o app/css/prod.css
+rm prodtmp.css
+
 
